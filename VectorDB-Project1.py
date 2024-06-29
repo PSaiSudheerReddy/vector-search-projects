69b009fae36a1a94a6f9d325a24e9396c97cb169
PSaiSudheerReddy <patilsaisudheer25@gmail.com>
2024-06-29 18:31:40 +0530

    initial commmit

---
+++ VectorDB-Project1.py
@@ -0,0 +1,51 @@
+import pymongo
+import requests
+
+client = pymongo.MongoClient("mongodb+srv://patilsaisudheer25:LK6Y1uHTWlS2sAp7@cluster0.dfqo0hy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client.sample_mflix
+collection = db.movies
+
+items= collection.find().limit(5)
+
+#for item in items:
+ # print(item)
+
+hf_token = "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"
+embedding_url = 'https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2'
+
+def generate_embedding(text: str) -> list[float]:
+
+  response = requests.post(
+    embedding_url,
+    headers={"Authorization": f"Bearer {hf_token}"},
+    json={"inputs": text})
+
+  if response.status_code != 200:
+    raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+
+  return response.json()
+
+
+# print(generate_embedding("Happy face is awesome"))
+# for doc in collection.find({'plot':{"$exists": True}}).limit(100):
+#   doc['plot_embedding_hf'] = generate_embedding(doc['plot'])
+#   collection.replace_one({'_id': doc['_id']}, doc)
+
+query = "Comedy characters"
+
+#print(generate_embedding(query))
+results = collection.aggregate([
+  {"$vectorSearch": {
+    "queryVector": generate_embedding(query),
+    "path": "plot_embedding_hf",
+    "numCandidates": 100,
+    "limit": 4,
+    "index": "PotSemanticSearch",
+      }}
+]);
+
+# results_list = list(results)
+# print("size of list returned.." + str(len(results_list)))
+
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
