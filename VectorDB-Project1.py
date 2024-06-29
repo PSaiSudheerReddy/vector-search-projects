91dab9aa584155ffa0d16f89e353c05c429880a3
PSaiSudheerReddy <74001025+PSaiSudheerReddy@users.noreply.github.com>
2024-06-29 18:40:45 +0530

    Update VectorDB-Project1.py

--- VectorDB-Project1.py
+++ VectorDB-Project1.py
@@ -1,7 +1,7 @@
 import pymongo
 import requests
 
-client = pymongo.MongoClient("mongodb+srv://patilsaisudheer25:LK6Y1uHTWlS2sAp7@cluster0.dfqo0hy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = pymongo.MongoClient("mongodb+srv://patilsaisudheer25:*****@cluster0.dfqo0hy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.sample_mflix
 collection = db.movies
 
@@ -10,19 +10,16 @@ items= collection.find().limit(5)
 #for item in items:
  # print(item)
 
-hf_token = "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"
+hf_token = "*******"
 embedding_url = 'https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2'
 
 def generate_embedding(text: str) -> list[float]:
-
   response = requests.post(
     embedding_url,
     headers={"Authorization": f"Bearer {hf_token}"},
     json={"inputs": text})
-
   if response.status_code != 200:
     raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
-
   return response.json()
 
 
@@ -33,7 +30,8 @@ def generate_embedding(text: str) -> list[float]:
 
 query = "Comedy characters"
 
-#print(generate_embedding(query))
+print(generate_embedding(query))
+
 results = collection.aggregate([
   {"$vectorSearch": {
     "queryVector": generate_embedding(query),
@@ -44,8 +42,8 @@ results = collection.aggregate([
       }}
 ]);
 
-# results_list = list(results)
-# print("size of list returned.." + str(len(results_list)))
+results_list = list(results)
+print("size of list returned.." + str(len(results_list)))
 
 for document in results:
     print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
